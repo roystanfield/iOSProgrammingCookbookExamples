7fc0b51a5e16ccda8cd99d92faabc53f05b5d685
Roy Stanfield <roystanfield@gmail.com>
2014-05-25 17:22:24 -0400

    Added a simple picker view tutorial

---
+++ 01/1.4-UIPickerView/1.4-UIPickerView/JRSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JRSAppDelegate.h
+//  1.4-UIPickerView
+//
+//  Created by Roy Stanfield on 5/25/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JRSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 01/1.4-UIPickerView/1.4-UIPickerView/ViewController.m
@@ -0,0 +1,84 @@
+//
+//  ViewController.m
+//  1.4-UIPickerView
+//
+//  Created by Roy Stanfield on 5/25/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
+@property (nonatomic, strong) UIPickerView *myPicker;
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.myPicker = [[UIPickerView alloc] init];
+    self.myPicker.dataSource = self;
+    self.myPicker.delegate = self;
+    self.myPicker.center = self.view.center;
+    [self.view addSubview:self.myPicker];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
+{
+    if ([pickerView isEqual:self.myPicker]) {
+        return 1;
+    }
+    
+    return 0;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component;
+{
+    if ([pickerView isEqual:self.myPicker]) {
+        return 10;
+    }
+    
+    return 0;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if ([pickerView isEqual:self.myPicker]) {
+        return [NSString stringWithFormat:@"Row %ld", (long)row + 1];
+    }
+    
+    return nil;
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
