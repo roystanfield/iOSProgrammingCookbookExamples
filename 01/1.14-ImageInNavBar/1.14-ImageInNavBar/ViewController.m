d9167e83362c7b588b52a142e41afab7231a3bd1
Roy Stanfield <roystanfield@gmail.com>
2014-05-26 15:22:59 -0400

    Getting an image into the title bar

---
+++ 01/1.14-ImageInNavBar/1.14-ImageInNavBar/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  1.14-ImageInNavBar
+//
+//  Created by Roy Stanfield on 5/26/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIImage *fbImage = [UIImage imageNamed:@"fb_logo_black.png"];
+    UIImageView *fbImageView = [[UIImageView alloc] initWithImage:fbImage];
+    
+    self.navigationItem.titleView = fbImageView;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
