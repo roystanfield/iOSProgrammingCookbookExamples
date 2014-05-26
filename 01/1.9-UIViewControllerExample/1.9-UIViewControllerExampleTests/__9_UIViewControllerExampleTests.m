93566a7ae3729e2071897f4bff34bf724b5c16b0
Roy Stanfield <roystanfield@gmail.com>
2014-05-26 11:32:46 -0400

    1.9 the viewController lesson.

---
+++ 01/1.9-UIViewControllerExample/1.9-UIViewControllerExampleTests/__9_UIViewControllerExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  __9_UIViewControllerExampleTests.m
+//  1.9-UIViewControllerExampleTests
+//
+//  Created by Roy Stanfield on 5/26/14.
+//  Copyright (c) 2014 Roy Stanfield. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __9_UIViewControllerExampleTests : XCTestCase
+
+@end
+
+@implementation __9_UIViewControllerExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
